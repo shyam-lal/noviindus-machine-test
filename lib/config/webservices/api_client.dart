e8053226cf518f755311a034d18e7d5320154136
shyam-lal <slal4252@gmail.com>
2025-09-12 20:57:35 +0530

    refactor: refactor body to send as form data.

--- lib/config/webservices/api_client.dart
+++ lib/config/webservices/api_client.dart
@@ -6,33 +6,42 @@ import 'package:novindus_machine_test/config/webservices/api_response.dart';
 import 'package:novindus_machine_test/repositories/auth_service.dart';
 
 class ApiClient {
-  static Future<ApiResponse<T>?> postData<T>({
-    required ApiRequest request,
-    required T Function(dynamic) fromJson,
-    bool? needsHeader,
-  }) async {
-    try {
-      final token = AuthService().token;
-      Map<String, String> headers = {
+static Future<ApiResponse<T>?> postData<T>({
+  required ApiRequest request,
+  required T Function(dynamic) fromJson,
+  bool? needsHeader,
+  bool formUrlEncoded = false,
+}) async {
+  try {
+    final token = AuthService().token;
+    Map<String, String> headers = {
+      if (formUrlEncoded)
+        'Content-Type': 'application/x-www-form-urlencoded'
+      else
         'Content-Type': 'application/json',
-        if (token != null && (needsHeader ?? true)) 'Authorization': 'Bearer $token',
-        ...?request.headers,
-      };
+      if (token != null && (needsHeader ?? true))
+        'Authorization': 'Bearer $token',
+      ...?request.headers,
+    };
 
-      final response = await http.post(
-        request.url!,
-        body: jsonEncode(request.body),
-        headers: headers,
-      );
+    final response = await http.post(
+      request.url!,
+      body: formUrlEncoded ? request.body : jsonEncode(request.body),
+      headers: headers,
+    );
 
-      if (response.statusCode == 200 || response.statusCode == 201) {
-        final jsonResponse = jsonDecode(response.body);
-        return ApiResponse(data: fromJson(jsonResponse));
-      } else {
-        return ApiResponse(error: "HTTP ERROR ${response.statusCode}");
-      }
-    } catch (e) {
-      return ApiResponse(error: e.toString());
+    print("POST ${request.url}");
+    print("Headers: $headers");
+    print("Body: ${request.body}");
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      final jsonResponse = jsonDecode(response.body);
+      return ApiResponse(data: fromJson(jsonResponse));
+    } else {
+      return ApiResponse(error: "HTTP ERROR ${response.statusCode}");
     }
+  } catch (e) {
+    return ApiResponse(error: e.toString());
   }
+}
 }
